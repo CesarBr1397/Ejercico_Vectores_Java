8606ec119774f7934fb1266655d5008c6ad8094f
Cesar Briseño <142867500+CesarBr1397@users.noreply.github.com>
2024-02-21 14:37:05 -0600

    Add files via upload

---
+++ Ejercicio_Vector.java
@@ -0,0 +1,70 @@
+package ejercicio_vector;
+import java.util.Scanner;
+
+public class Ejercicio_Vector {
+
+    public static void main(String[] args) {
+////////////////////////////////////////////Ejercicio Vector///////////////////////////////////////////
+//            int vector [ ] = new int [15];
+//            Scanner teclado = new Scanner(System.in);
+////            Cargar nuestro vector
+//        for (int i = 0; i <15; i++){
+//            System.out.println("Ingrese un numero para el vector: ");
+//            vector [i] = teclado.nextInt();
+//        }
+////        Recorrer y contar cuantos numeros 3 hay
+//        int cont = 0;
+//        for (int i = 0; i <15; i++){
+//            if (vector[i] == 3) {
+//            cont = cont + 1;
+//            }
+//        }
+//        System.out.println("La cantidad de números que hay en el vectore es: " + cont);
+
+//////////////////////////////////////////Ejercicio promedios//////////////////////////////////////////
+
+//Resolución mia
+
+//    Double matriz [ ] [ ] = new Double [4] [3];
+//    Scanner teclado = new Scanner (System.in);
+//    for ( int alumno = 0; alumno<4; alumno ++ ){
+//        for (int calificacion = 0; calificacion<3; calificacion ++){
+//            System.out.println("Ingrese la calificacion " + (calificacion+1) + " del alumno " + (alumno+1));
+//            matriz [alumno] [calificacion] = teclado.nextDouble();
+//            suma = suma + matriz [alumno] [calificacion];
+//        }
+//        matriz [alumno] [3] = suma / 3;
+//        suma = 0.0;
+//    }
+//        for (int alumno = 0; alumno<4; alumno++){ 
+//        System.out.println("Las calificaciones del alumno "  + (alumno+1) + " son: ");
+//    for (int calificacion = 0; calificacion<3; calificacion++){
+//        System.out.println(matriz [alumno] [calificacion]);
+//    } 
+//        System.out.println("El promedio del alumno " + (alumno +1) + " es: " + matriz [alumno] [3]);
+//}
+
+//Resolución clase
+
+//    Double matriz [ ] [ ] = new Double [4] [4];
+//    Scanner teclado = new Scanner (System.in);
+//    Double suma = 0.0;
+//    
+//    for ( int alumno = 0; alumno<4; alumno ++ ){
+//        for (int calificacion = 0; calificacion<3; calificacion ++){
+//            System.out.println("Ingrese la calificacion " + (calificacion+1) + " del alumno " + (alumno+1));
+//            matriz [alumno] [calificacion] = teclado.nextDouble();
+//            suma = suma + matriz [alumno] [calificacion];
+//        }
+//        matriz [alumno] [3] = suma / 3;
+//        suma = 0.0;
+//    }
+//    for (int alumno = 0; alumno<4; alumno++){ 
+//         System.out.println("Las notas del alumno n° " + alumno + " son: ");
+//    for (int calificacion = 0; calificacion<3; calificacion++){
+//        System.out.println("Nota N°: " + calificacion + "  " + matriz [alumno] [calificacion]);
+//    }
+//        System.out.println("El promedio de las notas es: " + matriz [alumno][3]);
+//}
+    }
+}
